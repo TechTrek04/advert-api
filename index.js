19567cab7c9310563f61c5db65cc2fddafcf28f4
steveapp531 <steveapp531@gmail.com>
2025-03-17 17:05:05 +0000

    1st commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose';
+import cors from "cors"
+
+
+
+await mongoose.connect(process.env.MONGO_URI);
+const connectionString = process.env.MONGO_URI;
+
+
+
+
+const app = express();
+
+mongoose.connect(connectionString).then(() => {
+    console.log('database connected')
+}).catch((err) => {
+    console.log(err)
+})
+
+const PORT = 8000;
+
+// Middleware to parse JSON requests
+app.use(cors())
+app.use(express.json());
+
+ 
+
+const port = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
