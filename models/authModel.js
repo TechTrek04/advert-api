0e98f15f8a2f64d35912c35cfde4e9536b4daf86
Ayesha <nheemsha18@gmail.com>
2025-03-24 09:33:09 +0000

    auth

---
+++ models/authModel.js
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+import normalize from "normalize-mongoose";
+
+const userSchema = new Schema({
+    username :{type: String, required: true, unique: true},
+    email :{type: String, required: true, unique: true},
+    password :{type: String, required: true}
+},{
+timestamps: true
+});
+//the id on postman
+userSchema.plugin(normalize);
+export const UserModel = model('User', userSchema)
